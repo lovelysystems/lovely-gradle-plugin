1ea217efc17f9fd538490215d4accdec9abf5555
dobe <bernddorn@gmail.com>
2019-12-02 23:31:45 +0100

    upgrade gradle and kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
     `maven-publish`
-    kotlin("jvm") version "1.2.50"
-    id("com.gradle.plugin-publish") version "0.9.10"
+    kotlin("jvm") version "1.3.50"
+    id("com.gradle.plugin-publish") version "0.10.1"
 }
 
 repositories {
@@ -17,7 +17,7 @@ val pluginId = "com.lovelysystems.gradle"
 
 gradlePlugin {
     plugins {
-        register(pluginId) {
+        create(pluginId) {
             id = pluginId
             implementationClass = "com.lovelysystems.gradle.LovelyGradlePlugin"
         }
@@ -25,11 +25,11 @@ gradlePlugin {
 }
 
 dependencies {
-    testCompile(gradleTestKit())
-    testCompile("junit:junit:4.12")
-    testCompile(kotlin("test"))
-    testCompile(kotlin("test-junit"))
-    testCompile("org.amshove.kluent:kluent:1.35")
+    testImplementation(gradleTestKit())
+    testImplementation("junit:junit:4.12")
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit"))
+    testImplementation("org.amshove.kluent:kluent:1.35")
 }
 
 publishing {
@@ -52,9 +52,8 @@ pluginBundle {
     description = "Gradle Plugins for Lovely Systems Projects"
     tags = listOf("git", "docker")
 
-    plugins {
-        register(pluginId) {
-            id = pluginId
+    (plugins) {
+        pluginId {
             displayName = "Lovely Systems Project Helpers"
             description = "Gradle Plugin for Lovely Systems Projects"
         }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,1 @@
 rootProject.name = "com.lovelysystems.gradle"
-enableFeaturePreview("STABLE_PUBLISHING")
