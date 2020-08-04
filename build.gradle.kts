8741daffc3f9d0b8e689dda4059377faaa528ab8
dobe <bernddorn@gmail.com>
2020-08-04 16:00:58 +0200

    upgrade publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     `kotlin-dsl`
     `maven-publish`
     kotlin("jvm") version "1.3.71"
-    id("com.gradle.plugin-publish") version "0.10.1"
+    id("com.gradle.plugin-publish") version "0.12.0"
 }
 
 repositories {
