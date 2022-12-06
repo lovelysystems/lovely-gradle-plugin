e34264da1e78b127e5e17f773d9934d0fc114802
Adam Kobor <28044849+adamkobor@users.noreply.github.com>
2022-12-06 11:16:24 +0100

    chore: update dependencies (#29)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
-    `java-gradle-plugin`
     `kotlin-dsl`
-    kotlin("jvm") version "1.4.31"
-    id("com.gradle.plugin-publish") version "0.15.0"
+    kotlin("jvm") version "1.7.10"
+    id("com.gradle.plugin-publish") version "1.1.0"
 }
 
 repositories {
@@ -19,16 +18,17 @@ gradlePlugin {
         create(pluginId) {
             id = pluginId
             implementationClass = "com.lovelysystems.gradle.LovelyGradlePlugin"
+            displayName = "Lovely Systems Project Helpers"
+            description = "Gradle Plugin for Lovely Systems Projects"
         }
     }
 }
 
 dependencies {
     testImplementation(gradleTestKit())
-    testImplementation("junit:junit:4.12")
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit"))
-    testImplementation("org.amshove.kluent:kluent:1.68")
+    testImplementation("org.amshove.kluent:kluent:1.72")
 }
 
 pluginBundle {
@@ -36,11 +36,4 @@ pluginBundle {
     vcsUrl = "https://github.com/lovelysystems/lovely-gradle-plugin"
     description = "Gradle Plugins for Lovely Systems Projects"
     tags = listOf("git", "docker")
-
-    (plugins) {
-        pluginId {
-            displayName = "Lovely Systems Project Helpers"
-            description = "Gradle Plugin for Lovely Systems Projects"
-        }
-    }
 }
