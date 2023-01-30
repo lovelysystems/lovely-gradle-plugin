aa29b8df19a3bef84f060bdce76b2b1cb8e80550
Harald Friessnegger <friessnegger@lovelysystems.com>
2023-01-30 16:18:12 +0100

    add lookahead to support hashes starting with a digit

--- src/test/kotlin/com/lovelysystems/gradle/PythonProjectTest.kt
+++ src/test/kotlin/com/lovelysystems/gradle/PythonProjectTest.kt
@@ -8,17 +8,17 @@ class PythonProjectTest {
     @Test
     fun testPep440Version() {
         // no release yet
-        pep440Version("gcbcdbe6") shouldBeEqualTo "0.0+gcbcdbe6"
+        pep440Version("757ea77") shouldBeEqualTo "0.0+757ea77"
         // dirty commit w/o release
-        pep440Version("gcbcdbe6.dirty") shouldBeEqualTo "0.0+gcbcdbe6.dirty"
+        pep440Version("757ea77.dirty") shouldBeEqualTo "0.0+757ea77.dirty"
         // proper release
         pep440Version("1.2") shouldBeEqualTo "1.2"
         // release with additional commits
-        pep440Version("1.2-2-gcbcdbe6") shouldBeEqualTo "1.2+2-gcbcdbe6"
+        pep440Version("1.2-2-757ea77") shouldBeEqualTo "1.2+2-757ea77"
         // release with additional commits and dirty state
-        pep440Version("1.2-2-gcbcdbe6.dirty") shouldBeEqualTo "1.2+2-gcbcdbe6.dirty"
+        pep440Version("1.2-2-757ea77.dirty") shouldBeEqualTo "1.2+2-757ea77.dirty"
         // unlimited release segments
-        pep440Version("1.2.3.4.5-4-gcbcdbe6.dirty") shouldBeEqualTo "1.2.3.4.5+4-gcbcdbe6.dirty"
+        pep440Version("1.2.3.4.5-4-757ea77.dirty") shouldBeEqualTo "1.2.3.4.5+4-757ea77.dirty"
         // proper pep440 version not converted
         pep440Version("1.2.3.dev0") shouldBeEqualTo "1.2.3.dev0"
         pep440Version("1.2.3.a1") shouldBeEqualTo "1.2.3.a1"
