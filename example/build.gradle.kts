586d3eb129984089004931720bb787e0d5e2106d
Lukas-Kaufmann <kaufmann@lovelysystems.com>
2024-09-12 11:14:59 +0200

    add example tasks

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.lovelysystems.gradle.S3DownloadFile
+import com.lovelysystems.gradle.S3UploadDirectory
+
 plugins {
     base
     id("com.lovelysystems.gradle")
@@ -7,7 +10,7 @@ val awsProfile = "lovely"
 lovely {
     gitProject()
     pythonProject("python3")
-    awsProject(awsProfile)
+    awsProject(awsProfile, "eu-central-1")
 
     // build the default stage "" and the "base" stage defined in the Dockerfile
     dockerProject(
@@ -20,6 +23,21 @@ lovely {
     }
 }
 
+/**
+ * The S3 tasks don't actually work as the buckets/objects don't exist.
+ * They are there to demonstrate how to use them.
+ */
+val downloadFile by tasks.registering(S3DownloadFile::class) {
+    bucket.set("some-bucket")
+    targetFile.set(File(buildDir, "samples.zip"))
+    key.set("samples.zip")
+}
+
+val uploadContent by tasks.registering(S3UploadDirectory::class) {
+    bucket = "some-bucket"
+    sourceDirectory = File(buildDir, "some-content")
+    prefix = "some-target-prefix"
+}
 
 tasks["prepareDockerImage"].dependsOn("venv")
 
